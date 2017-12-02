2a5a609f2395e3331a52241465fcb3197b97490e
StefanMUC <chocolatey-git_17@green-sparklet.de>
2017-12-02 18:38:15 +0100

    config.disablebeep: Add 1062 as valid exit code on uninstall for automated testing (without restart in between)

--- config/disablebeep/tools/chocolateyuninstall.ps1
+++ config/disablebeep/tools/chocolateyuninstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
 $statementsToRun = "/C `sc config beep start= system`""
-$validExitCodes= @(0, 1061)
+$validExitCodes= @(0, 1061, 1062)
 
 Start-ChocolateyProcessAsAdmin $statementsToRun cmd -validExitCodes $validExitCodes
