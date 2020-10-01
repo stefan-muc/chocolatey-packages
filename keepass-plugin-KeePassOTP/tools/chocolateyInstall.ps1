e64ecdf8ad763db5c2234ddcf0b7aca20c23652a
StefanMUC <chocolatey-git_17@green-sparklet.de>
2020-10-01 12:45:06 +0200

    [KeePassOTP] Add version 0.16

---
+++ keepass-plugin-KeePassOTP/tools/chocolateyInstall.ps1
@@ -0,0 +1,57 @@
+﻿# script based on tunisiano187, released under AGPL-3.0 License
+# source: https://github.com/tunisiano187/Chocolatey-packages/blob/master/automatic/keepass-plugin-kpscript/tools/chocolateyInstall.ps1
+
+# powershell v2 compatibility
+$psVer = $PSVersionTable.PSVersion.Major
+if ($psver -ge 3) {
+  function Get-ChildItemDir {Get-ChildItem -Directory $args}
+} else {
+  function Get-ChildItemDir {Get-ChildItem $args}
+}
+$packageName = $env:ChocolateyPackageName
+$packageSearch = 'KeePass Password Safe'
+$typName = 'KeePassOTP.plgx'
+$url = 'https://github.com/Rookiestyle/KeePassOTP/releases/download/v0.16/KeePassOTP.plgx'
+$checksum = '72F254D59A331F8B8316212DDC246E933DDC2FBB732EB6902B25DA9748EE9D89'
+$checksumType = 'sha256'
+try {
+# search registry for location of installed KeePass
+$regPath = Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                    'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                    'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
+                            -ErrorAction:SilentlyContinue `
+           | Where-Object {$_.DisplayName -like "$packageSearch*" `
+                           -and `
+                           $_.DisplayVersion -ge 2.29 `
+                           -and `
+                           $_.DisplayVersion -lt 3.0 } `
+           | ForEach-Object {$_.InstallLocation}
+$installPath = $regPath + "Plugins\"
+# search $env:ChocolateyBinRoot for portable install
+if (! $installPath) {
+  Write-Verbose "$($packageSearch) not found installed."
+  $binRoot = Get-BinRoot
+  $portPath = Join-Path $binRoot "keepass"
+  $installPath = Get-ChildItemDir $portPath* -ErrorAction SilentlyContinue
+}
+if (! $installPath) {
+  Write-Verbose "$($packageSearch) not found in $($env:ChocolateyBinRoot)"
+  throw "$($packageSearch) location could not be found."
+}
+$pluginPath = $installPath
+$installFile = Join-Path $pluginPath $typName
+# download PLGX file into Plugins dir
+Get-ChocolateyWebFile -PackageName "$packageName" `
+                             -Url "$url" `
+                             -FileFullPath  "$installFile" `
+                             -Checksum "$checksum" `
+                             -ChecksumType "$checksumType"
+if ( Get-Process -Name "KeePass" `
+                 -ErrorAction SilentlyContinue ) {
+  Write-Warning "$($packageSearch) is currently running. Plugin will be available at next restart of $($packageSearch)."
+} else {
+  Write-Output "$($packageName) will be loaded the next time KeePass is started."
+  Write-Output "Please note this plugin may require additional configuration. Look for a new entry in KeePass' Menu -> Tools"
+}} catch {
+  throw $_.Exception
+}
---
+++ keepass-plugin-KeePassOTP/tools/chocolateyUninstall.ps1
@@ -0,0 +1,45 @@
+# script based on tunisiano187, released under AGPL-3.0 License
+# source: https://github.com/tunisiano187/Chocolatey-packages/blob/master/automatic/keepass-plugin-kpscript/tools/chocolateyInstall.ps1
+
+# powershell v2 compatibility
+$psVer = $PSVersionTable.PSVersion.Major
+if ($psver -ge 3) {
+  function Get-ChildItemDir {Get-ChildItem -Directory $args}
+} else {
+  function Get-ChildItemDir {Get-ChildItem $args}
+}
+$packageName = 'keepass-plugin-keepassotp'
+$typName = 'KeePassOTP.plgx'
+$packageSearch = 'KeePass Password Safe'
+try {
+# search registry for installed KeePass
+$regPath = Get-ItemProperty -Path @('HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                    'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                    'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*') `
+                            -ErrorAction:SilentlyContinue `
+           | Where-Object {$_.DisplayName -like "$packageSearch*"} `
+           | ForEach-Object {$_.InstallLocation}
+$installPath = $regPath + "Plugins\"
+# search $env:ChocolateyBinRoot for portable install
+if (! $installPath) {
+  Write-Verbose "$($packageSearch) not found in registry."
+  $binRoot = Get-BinRoot
+  $portPath = Join-Path $binRoot "keepass"
+  $installPath = Get-ChildItemDir $portPath* -ErrorAction SilentlyContinue
+}
+if (! $installPath) {
+  Write-Verbose "$($packageSearch) not found in $($env:ChocolateyBinRoot)"
+  throw "$($packageSearch) install location could not be found."
+}
+$pluginPath = $installPath
+$installFile = Join-Path $pluginPath $typName
+Remove-Item -Path $installFile `
+            -Force `
+            -ErrorAction Continue
+if ( Get-Process -Name "KeePass" `
+                 -ErrorAction SilentlyContinue ) {
+  Write-Warning "$($packageSearch) is running. $($packageName) will be removed at next restart of $($packageSearch)."
+}
+} catch {
+  throw $_.Exception
+}
