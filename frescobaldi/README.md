63ab55763c75ce72aefc5defd3d597bc049311ed
StefanMUC <chocolatey-git_17@green-sparklet.de>
2021-05-06 23:13:24 +0200

    [frescobaldi] Add version 3.1.3

---
+++ frescobaldi/README.md
@@ -0,0 +1,9 @@
+# Frescobaldi
+
+Frescobaldi is a LilyPond sheet music text editor. It aims to be powerful, yet
+lightweight and easy to use. Frescobaldi is Free Software, freely available
+under the General Public License.
+
+## Package Issues
+
+If this package isn't up-to-date for some days, [Create an issue](https://github.com/stefan-muc/chocolatey-packages/issues/new)
---
+++ frescobaldi/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url        = 'https://github.com/frescobaldi/frescobaldi/releases/download/v3.1.3/Frescobaldi.Setup.3.1.3.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  softwareName  = 'Frescobaldi Full Windows installer'
+
+  checksum      = 'D36FB76BA59A157F6E7CFA5C6DD36EF899527566A3B15BDB855DA852886E08F3'
+  checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
+
+  silentArgs    = "/VERYSILENT"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
