d6520eaa3092837d6271ebfcee780e2ce1043bb7
StefanMUC <chocolatey-git_17@green-sparklet.de>
2021-07-31 19:57:40 +0200

    [dmxcontrol3] Add version 3.2.2.339

---
+++ dmxcontrol3/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'DMXControl_3.2.2.339_Setup.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  file         = $fileLocation
+
+  softwareName  = 'DMXControl 3'
+
+  checksum      = '268FCDBCA243A279A2BCA77451F7A5F31C2083BF1F17033C37C4260E3B8A9888'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
