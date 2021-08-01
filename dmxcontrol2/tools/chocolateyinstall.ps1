f66f1d5377fefe8e25ee4ebf44266abcc1c455cd
StefanMUC <chocolatey-git_17@green-sparklet.de>
2021-08-01 19:21:40 +0200

    [dmxcontrol2] Add version 2.12.2

---
+++ dmxcontrol2/tools/chocolateyinstall.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'DMXControl_2.12.2_Setup.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  file         = $fileLocation
+
+  softwareName  = 'DMXControl 2.12.2'
+
+  checksum      = 'BFC3671A25BF9F251B7404C07420C7F0009C1D5FE8A2EDF02C147D950D0BD65B'
+  checksumType  = 'sha256'
+
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
