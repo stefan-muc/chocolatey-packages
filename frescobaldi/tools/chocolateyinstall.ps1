f80ffb89c459b23c75bbc3906619c5921c4c22ca
StefanMUC <chocolatey-git_17@green-sparklet.de>
2023-05-15 09:37:05 +0200

    [frescobaldi] Update to 3.3

--- frescobaldi/tools/chocolateyinstall.ps1
+++ frescobaldi/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url        = 'https://github.com/frescobaldi/frescobaldi/releases/download/v3.2/Frescobaldi.Setup.3.2.exe'
+$url        = 'https://github.com/frescobaldi/frescobaldi/releases/download/v3.3.0/Frescobaldi.Setup.3.3.0.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -12,7 +12,7 @@ $packageArgs = @{
 
   softwareName  = 'Frescobaldi Full Windows installer'
 
-  checksum      = '760B5A4A15BEE8AE79A0BDC1869C16F9C54E292C1020CAA3706A5ACDFAB4D6C9'
+  checksum      = '5547aa06f078be27bda1c2199a8bd143a6e4dd48944dbd3f9d85e762e7e69f8d'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
 
   silentArgs    = "/VERYSILENT"
